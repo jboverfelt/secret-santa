86585f834d0518767bcc6b365e1e284acd625457
Justin Overfelt <joverfelt@gmail.com>
2013-12-31 10:55:03 -0500

    more wishlist routes and session info

--- src/secret_santa/routes/wishlist.clj
+++ src/secret_santa/routes/wishlist.clj
@@ -7,17 +7,29 @@
             [noir.response :as resp]
             [noir.session :as session]))
 
-(defn wishlist []
+(defn show-wishlist []
   (let [my-wishlist (wishlist/get-wishlist-by-user (session/get :user))
         child-wishlist (wishlist/get-child-wishlist-by-user (session/get :user))]
-    (layout/common (clo/render-resource "templates/wishlist.mustache" {:my my-wishlist :child child-wishlist}))))
+    (layout/render "templates/wishlist.mustache" {:my my-wishlist :child child-wishlist})))
 
 (defn create-wishlist [text]
   (let [user (user/find-user-by-email (session/get :user))
-        user-id (:_id user)])
+        user-id (:_id user)]
     (wishlist/new-wishlist {:text text :user_id user-id})
-    (resp/redirect "/wishlist"))
+    (resp/redirect "/wishlist")))
+
+(defn edit-wishlist []
+  (let [wishlist (wishlist/get-wishlist-by-user (session/get :user))]
+    (layout/render "templates/edit-wishlist.mustache" {:my wishlist})))
+
+(defn update-wishlist [id text]
+  (let [wishlist (wishlist/get-wishlist-by-id id)
+        updated (assoc wishlist :text text)]
+    (wishlist/update-wishlist updated)
+    (resp/redirect "/wishlist")))
 
 (defroutes wishlist-routes
-  (GET "/wishlist" [] (wishlist))
+  (GET "/wishlist" [] (show-wishlist))
+  (GET "/wishlist/edit" [] (edit-wishlist))
+  (POST "/wishlist/edit" [id text] (update-wishlist))
   (POST "/wishlist" [text] (create-wishlist text)))
--- src/secret_santa/views/layout.clj
+++ src/secret_santa/views/layout.clj
@@ -1,5 +1,9 @@
 (ns secret-santa.views.layout
-  (:require [clostache.parser :as clo]))
+  (:require [clostache.parser :as clo]
+            [noir.session :as session]))
 
 (defn common [body]
-    (clo/render-resource "templates/layout.mustache" {:body body}))
+    (clo/render-resource "templates/layout.mustache" {:body body :user (session/get :user)}))
+
+(defn render [template data]
+  (common (clo/render-resource template data)))
