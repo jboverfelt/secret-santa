3a7f0c43b3407b2208cc772296ef8d7b2bd3bf34
Justin Overfelt <joverfelt@gmail.com>
2013-12-31 10:28:47 -0500

    adding create routes to wishlist

--- src/secret_santa/routes/wishlist.clj
+++ src/secret_santa/routes/wishlist.clj
@@ -2,10 +2,22 @@
   (:require [compojure.core :refer :all]
             [clostache.parser :as clo]
             [secret-santa.views.layout :as layout]
-            [secret-santa.models.wishlist :as wishlist]))
+            [secret-santa.models.wishlist :as wishlist]
+            [secret-santa.models.user :as user]
+            [noir.response :as resp]
+            [noir.session :as session]))
 
 (defn wishlist []
-  (layout/common (clo/render-resource "templates/wishlist.mustache" {})))
+  (let [my-wishlist (wishlist/get-wishlist-by-user (session/get :user))
+        child-wishlist (wishlist/get-child-wishlist-by-user (session/get :user))]
+    (layout/common (clo/render-resource "templates/wishlist.mustache" {:my my-wishlist :child child-wishlist}))))
+
+(defn create-wishlist [text]
+  (let [user (user/find-user-by-email (session/get :user))
+        user-id (:_id user)])
+    (wishlist/new-wishlist {:text text :user_id user-id})
+    (resp/redirect "/wishlist"))
 
 (defroutes wishlist-routes
-  (GET "/wishlist" [] (wishlist)))
+  (GET "/wishlist" [] (wishlist))
+  (POST "/wishlist" [text] (create-wishlist text)))
