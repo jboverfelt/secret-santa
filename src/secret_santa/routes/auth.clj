43657a0fc42eef67a1f6dc9993577e425c05af8e
Justin Overfelt <joverfelt@gmail.com>
2014-03-02 11:00:48 -0500

    adding check to ensure users are invited before they can register

--- src/secret_santa/routes/auth.clj
+++ src/secret_santa/routes/auth.clj
@@ -1,5 +1,6 @@
 (ns secret-santa.routes.auth
   (:require [secret-santa.models.user :as user]
+            [secret-santa.models.invited-user :as invite]
             [secret-santa.views.layout :as layout]
             [compojure.core :refer :all]
             [noir.util.route :refer [restricted]]
@@ -13,10 +14,20 @@
 (defn show-login-page []
   (layout/render "templates/login.mustache" {}))
 
+(defn invited? [email]
+  (invite/find-invited-user-by-email email))
+
 (defn create-user [email password fullname]
   (let [user (user/create-user email password fullname)]
     (session/put! :user (:_id user))
     (resp/redirect "/")))
+
+(defn check-invite [email password fullname]
+  (if (invited? email)
+    (create-user email password fullname)
+    (do
+      (session/flash-put! :error "You must be invited by an administrator before registering")
+      (show-registration-page))))
 
 (defn authenticate-user [email password]
   (let [user (user/find-user-by-email email)
@@ -37,7 +48,7 @@
 
 (defroutes auth-routes
   (GET "/register" [] (show-registration-page))
-  (POST "/register" [email password fullname] (create-user email password fullname))
+  (POST "/register" [email password fullname] (check-invite email password fullname))
   (GET "/login" [] (show-login-page))
   (POST "/login" [email password] (authenticate-user email password))
   (GET "/logout" [] (logout)))
