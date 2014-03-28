9cff5c23cb77703eb294d8cb69de337eecbd6688
Justin Overfelt <joverfelt@gmail.com>
2014-03-27 21:09:53 -0400

    adding some tag filtering for the markdown parser

--- src/secret_santa/helpers/presenters.clj
+++ src/secret_santa/helpers/presenters.clj
@@ -1,5 +1,12 @@
 (ns secret-santa.helpers.presenters
   (:require [markdown.core :as md]))
 
+(defn remove-script [text state]
+  [(clojure.string/replace text #"<script>" "") state])
+
+(defn remove-img [text state]
+  [(clojure.string/replace text #"img" "") state])
+
 (defn md->html [text]
-  (md/md-to-html-string text))
+  (md/md-to-html-string text :custom-transformers [remove-script remove-img]))
+
