dd0a4a6d5425cbc316e04ce0b78c1655901980cc
Justin Overfelt <joverfelt@gmail.com>
2014-07-04 12:19:02 -0400

    update deps

--- project.clj
+++ project.clj
@@ -3,16 +3,16 @@
   :url "https://github.com/jboverfelt/secret-santa"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/core.logic "0.8.7"]
-                 [compojure "1.1.6"]
+                 [compojure "1.1.8"]
                  [ring-anti-forgery "0.3.0"]
-                 [lib-noir "0.8.1"]
-                 [clj-time "0.6.0"]
+                 [lib-noir "0.8.4"]
+                 [clj-time "0.7.0"]
                  [clojurewerkz/mailer "1.0.0"]
                  [com.novemberain/monger "1.7.0"]
-                 [autoclave "0.1.6"]
-                 [markdown-clj "0.9.43"]
-                 [de.ubercode.clostache/clostache "1.3.1"]
-                 [ring-server "0.3.0"]]
+                 [autoclave "0.1.7"]
+                 [markdown-clj "0.9.44"]
+                 [de.ubercode.clostache/clostache "1.4.0"]
+                 [ring-server "0.3.1"]]
   :plugins [[lein-ring "0.8.7"]]
   :ring {:handler secret-santa.handler/app
          :init secret-santa.handler/init
